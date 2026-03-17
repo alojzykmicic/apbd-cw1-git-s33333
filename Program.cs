dddc957d7e6d40bc6e62d1268ad9cd937f069246
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-17 18:02:03 +0100

    Added error for empty input

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Napisz cos: ");
 
 string input  = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Błąd: nie wprowadzono danych");
+    return;
+}
+Console.WriteLine("Wprowadzono dane poprawnie");
